200d1662e0a45234a7bc208055eccf85b1a28cc3
Aline LEGENDRE <legendrealine@gmail.com>
2023-07-20 11:00:59 +0200

    added contact form with flash message

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,43 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use App\Repository\ProjectsRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProjectsRepository $projectsRepository): Response
+    public function index(ProjectsRepository $projectsRepository, Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('my_portfolio@gmail.com')
+                ->subject('Demande de contact sur votre site de ' . $contactFormData['email'])
+                ->html($contactFormData['email'] . ' vous a envoyé le message suivant: ' . $contactFormData['message']);
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Votre message a été envoyé');
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('home/index.html.twig', [
             'projects' => $projectsRepository->findBy([], ['id' => 'DESC']),
+            'form' => $form
         ]);
     }
 }
